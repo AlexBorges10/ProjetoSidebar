2396cfafb5503d1a9783b2d87b9e6d2263f02830
Alex Borges Dario <alexborges486@gmail.com>
2022-03-18 11:26:05 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirnav(){
+    document.getElementById("menuoculto").style.width="250px";
+    document.getElementById("principal").style.marginleft="250px";
+
+}
+function fecharnav(){
+    document.getElementById("menuoculto").style.width="0";
+    document.getElementById("principal").style.marginleft="0";
+}
